6bbcdf9bb159e994233e17cbdb6ccd102359b970
Catalin Catana <catana@adobe.com>
2023-08-03 00:18:21 +0300

    deployments

--- jenkins/startPipeline.groovy
+++ jenkins/startPipeline.groovy
@@ -19,28 +19,29 @@ pipeline {
                 }
             }
         }
-        stage('Increment Version') {
-            steps {
-                    // read VERSION file and increment the version
-                    script {
-                        def version = readFile('VERSION').trim()
-                        def versionParts = version.split('\\.')
-                        def major = versionParts[0].toInteger()
-                        def minor = versionParts[1].toInteger()
-                        def patch = versionParts[2].toInteger()
-                        patch++
-                        def newVersion = "${major}.${minor}.${patch}"
-                        writeFile file: 'VERSION', text: newVersion
-                        echo "New version is ${newVersion}"
-                        withCredentials([sshUserPrivateKey(credentialsId: 'GITHUB_SSH_KEY', keyFileVariable: 'GITHUB_SSH_KEY')]) {
-
-                            sh 'git config --global user.email "Admin@me.com"\n' +
-                                '  git config --global user.name "Admine Me"'
-                            sh 'git add .; git commit -m "version increased";'
-                            sh 'GIT_SSH_COMMAND="ssh -i $GITHUB_SSH_KEY" git push origin HEAD:main '
-                        }
-                }
-            }
-        }
+        // If we use this, we will enter a loop of deployments
+//        stage('Increment Version') {
+//            steps {
+//                    // read VERSION file and increment the version
+//                    script {
+//                        def version = readFile('VERSION').trim()
+//                        def versionParts = version.split('\\.')
+//                        def major = versionParts[0].toInteger()
+//                        def minor = versionParts[1].toInteger()
+//                        def patch = versionParts[2].toInteger()
+//                        patch++
+//                        def newVersion = "${major}.${minor}.${patch}"
+//                        writeFile file: 'VERSION', text: newVersion
+//                        echo "New version is ${newVersion}"
+//                        withCredentials([sshUserPrivateKey(credentialsId: 'GITHUB_SSH_KEY', keyFileVariable: 'GITHUB_SSH_KEY')]) {
+//
+//                            sh 'git config --global user.email "Admin@me.com"\n' +
+//                                '  git config --global user.name "Admine Me"'
+//                            sh 'git add .; git commit -m "version increased";'
+//                            sh 'GIT_SSH_COMMAND="ssh -i $GITHUB_SSH_KEY" git push origin HEAD:main '
+//                        }
+//                }
+//            }
+//        }
     }
 }
