aab04d50f8b69435c88bc457c663720ad0abc8f6
Catalin Catana <catana@adobe.com>
2023-08-03 00:31:04 +0300

    running pytest on build - fail test

--- tests/unit/webapp/test_root.py
+++ tests/unit/webapp/test_root.py
@@ -2,5 +2,5 @@
 def test_root_path(app, client):
     res = client.get('/')
     assert res.status_code == 200
-    expected = "Glad to see you again"
+    expected = "Glad to see you again-fail"
     assert expected == res.get_data(as_text=True)
