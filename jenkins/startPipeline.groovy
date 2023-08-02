f38a250a448a5b5cc7b4c6079a2820a2dc17e100
Catalin Catana <catana@adobe.com>
2023-08-02 23:50:44 +0300

    wip - deployment pipelines

--- jenkins/startPipeline.groovy
+++ jenkins/startPipeline.groovy
@@ -29,6 +29,8 @@ pipeline {
                     def newVersion = "${major}.${minor}.${patch}"
                     writeFile file: 'VERSION', text: newVersion
                     echo "New version is ${newVersion}"
+                    sh 'git config --global user.email "Admin@me.com"\n' +
+                            '  git config --global user.name "Admine Me"'
                     sh 'git add .; git commit -m "version increased"; git push origin main'
                 }
             }
